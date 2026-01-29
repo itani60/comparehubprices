40d0340e550bef877262c4dffb0ecc914ea0719b
itani60 <itani2015@gmail.com>
2026-01-29 23:29:35 +0200

    m

--- chat-hub/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/chat-hub/',
-  plugins: [react()],
-  publicDir: 'assets'
-});
